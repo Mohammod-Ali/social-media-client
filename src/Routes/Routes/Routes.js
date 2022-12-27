9963c79f48fb20a1c9645a878802ecbb270cc53b
Mohammod Ali <mohammodali651@gmail.com>
2022-12-27 20:18:31 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom"
+import Main from "../../Layout/Main"
+import About from "../../Pages/About/About"
+import Home from "../../Pages/Home/Home/Home"
+import Login from "../../Pages/Login/Login"
+import Media from "../../Pages/Media/Media"
+import Message from "../../Pages/Message/Message"
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/media',
+                element: <Media></Media>
+            },
+            {
+                path: '/message',
+                element: <Message></Message>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            }
+        ]
+    }
+])
